c56d5392be1813a7027f0a253f01435b2c76e534
tohrashabh <114815314+tohrashabh@users.noreply.github.com>
2022-10-01 19:17:55 +0530

    Add files via upload

---
+++ AverageMarks.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class AverageMarks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=s.nextInt();
+		System.out.println(str.charAt(0));
+		System.out.println((a+b+c)/3);
+	}
+
+}
